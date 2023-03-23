0873f23881474d7db0bf8bb246bcfa7723307f11
Virgil Clyne <Virgil@nanocat.me>
2023-03-23 20:58:56 +0800

    Revert "test(BiliBili.Global): Caches"
    This reverts commit c50c37da229ab74b9f7947fcd3b207d40ee0de72.

--- js/BiliBili.Global.request.beta.js
+++ js/BiliBili.Global.request.beta.js
@@ -1,7 +1,7 @@
 /*
 README:https://github.com/VirgilClyne/BiliBili
 */
-const $ = new Env("📺 BiliBili:Global v0.2.9(3) request.beta");
+const $ = new Env("📺 BiliBili:Global v0.2.9(2) request.beta");
 const URL = new URLs();
 const DataBase = {
 	"Enhanced":{
@@ -32,7 +32,7 @@ let $response = undefined;
 
 /***************** Processing *****************/
 (async () => {
-	const { Settings, Configs } = setENV("BiliBili", "Global", DataBase);
+	const { Settings, Caches, Configs } = setENV("BiliBili", "Global", DataBase);
 	switch (Settings.Switch) {
 		case "true":
 		default:
@@ -106,8 +106,7 @@ let $response = undefined;
 													rawBody = PlayViewReq.toBinary(data);
 													let epid = data?.epid?.toString();
 													//$.log(`🚧 ${$.name}`, `epid: ${epid}`, "");
-													// 从缓存中获取可用的地区
-													if (getENV("BiliBili", "Global", DataBase).Caches?.ep?.[epid]) {
+													if (Caches?.ep?.[epid]) {
 														//$.log(`🚧 ${$.name}`, ` Caches.ep[epid]: ${Caches.ep[epid]}`, "");
 														let availableLocales = Caches.ep[epid].filter(locale => Settings?.Locales.includes(locale));
 														$.log(`🚧 ${$.name}`, `availableLocales: ${availableLocales}`, "");	
@@ -185,8 +184,7 @@ let $response = undefined;
 								case "pgc/player/web/playurl/html5": // 番剧-播放地址-web-HTML5
 									let epid = url?.params?.ep_id;
 									$.log(`🚧 ${$.name}`, `epid: ${epid}`, "");
-									// 从缓存中获取可用的地区
-									if (getENV("BiliBili", "Global", DataBase).Caches?.ep?.[epid]) {
+									if (Caches?.ep?.[epid]) {
 										let availableLocales = Caches.ep[epid].filter(locale => Settings?.Locales.includes(locale));
 										$.log(`🚧 ${$.name}`, `availableLocales: ${availableLocales}`, "");
 										$request = ReReqeust($request, Settings.Proxies[availableLocales[Math.floor(Math.random() * availableLocales.length)]]); // 随机用一个
@@ -211,8 +209,7 @@ let $response = undefined;
 									};
 									break;
 								case "pgc/view/web/season": // 番剧-内容-web
-									// 从缓存中获取可用的地区
-									if (getENV("BiliBili", "Global", DataBase).Caches.AccessKey) {
+									if (Caches.AccessKey) {
 										// https://github.com/ipcjs/bilibili-helper/blob/user.js/packages/unblock-area-limit/src/api/biliplus.ts
 									} else {
 										let responses = await mutiFetch($request, Settings.Proxies, Settings.Locales);
