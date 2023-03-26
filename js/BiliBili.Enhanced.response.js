588af21d18083679355c7c53648d82aeb4291036
Virgil Clyne <Virgil@nanocat.me>
2023-03-26 23:23:45 +0800

    fix(Enhanced.response): $.getjson()
    更新 BiliBili.Enhanced.response.js

--- js/BiliBili.Enhanced.response.js
+++ js/BiliBili.Enhanced.response.js
@@ -1,7 +1,7 @@
 /*
 README:https://github.com/VirgilClyne/BiliBili
 */
-const $ = new Env("📺 BiliBili:Enhanced v0.1.3(3) response");
+const $ = new Env("📺 BiliBili:Enhanced v0.1.3(4) response");
 const URL = new URLs();
 const DataBase = {
 	"Enhanced":{
@@ -57,7 +57,7 @@ for (const [key, value] of Object.entries($response.headers)) {
 						case "app.biliapi.net":
 							// 先保存一下AccessKey
 							if (url?.params?.access_key) {
-								let newCaches = $.getjson("@BiliBili.Global.Caches");
+								let newCaches = $.getjson("@BiliBili.Global.Caches", {});
 								newCaches.AccessKey = url.params.access_key; // 总是刷新
 								$.log(`newCaches = ${JSON.stringify(newCaches)}`);
 								let isSave = $.setjson(newCaches, "@BiliBili.Global.Caches");
